c3da4823ded06cee72d5f46a80f11ecd4de8adb3
Jesse Harrah <defiledspec@gmail.com>
2023-06-22 13:22:38 -0400

    Heroku died :(

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,2 @@
 // alert('This site has been moved to http://defiledspec.herokuapp.com/')
-setTimeout(() => window.location.replace('http://defiledspec.herokuapp.com/'), 2500)
+// setTimeout(() => window.location.replace('http://defiledspec.herokuapp.com/'), 2500)
